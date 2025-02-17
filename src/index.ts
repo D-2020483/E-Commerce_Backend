1272953bf4475d93a5ad502ed2e5cabb97baf335
Dinithi Imalsha Weerasingha <is2020483@mgt.rjt.ac.lk>
2025-02-17 23:10:58 +0530

    updates

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ import { paymentsRouter } from "./api/payment";
 const app = express();
 
 app.use(cors({ 
-    origin: "http://localhost:5173" ,
+    origin: "https://fed-storefront-frontend-dinithi.netlify.app" ,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']}));
@@ -37,4 +37,5 @@ app.use(globalErrorHandlingMiddleware);
 
 
 connectDB();
-app.listen(3000, () => console.log(`Server running on port ${3000}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
